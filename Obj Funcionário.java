c8cbbfdd19755eb7476547af92252b9b6aa76a19
Caio Bispo dos Reis <165906386+CaioBreis@users.noreply.github.com>
2024-04-15 23:19:13 -0300

    Create Obj Funcionário.java

---
+++ Obj Funcionário.java
@@ -0,0 +1,51 @@
+package enti;
+
+public class teste {
+    protected String nome;
+    protected double salario;
+    protected double valorH;
+
+    public teste(String nome, double salario, double valorH) {
+        this.nome = nome;
+        this.salario = salario;
+        this.valorH = valorH;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getValorH() {
+        return valorH;
+    }
+
+    public void setValorH(double valorH) {
+        this.valorH = valorH;
+    }
+
+    public double calcularSalario() {
+        return salario * valorH;
+    }
+
+    @Override
+    public String toString() {
+        return "teste{" +
+                "Nome='" + nome + '\'' +
+                ", Valor=" + salario +
+                ", valor da Hora" + valorH +
+                ", valor pra receber=" + calcularSalario() +
+                '}';
+    }
+}
