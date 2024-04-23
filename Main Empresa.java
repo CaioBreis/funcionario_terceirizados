334bb011e08c473a3062071a2217965f3ee47bc1
Caio Bispo dos Reis <165906386+CaioBreis@users.noreply.github.com>
2024-04-23 01:49:25 -0300

    Update and rename Main.java to Main Empresa.java

--- Main Empresa.java
+++ Main Empresa.java
@@ -5,7 +5,7 @@ package main;
 
     import java.util.Scanner;
 
-    public class main {
+    public class empresa {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
 
